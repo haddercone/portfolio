03e2f8900e6d352d9910bb72f192d329b541d955
Robin Singh <44robin.rs@gmail.com>
2023-06-27 18:31:24 +0530

    about and skill section added

---
+++ index.js
@@ -0,0 +1,28 @@
+const domElements = document.documentElement;
+document.querySelector('body').style.background="linear-gradient(-45deg,black, #173969)"
+domElements.style.color = 'white';
+
+
+let elements = document.querySelectorAll(".card")
+
+const observer = new IntersectionObserver((entries, observer) => {
+    console.log(entries);
+    entries.forEach((entry) => {
+        entry.target.classList.toggle("slide", entry.isIntersecting);
+        if(entry.isIntersecting) observer.unobserve(entry.target);
+        
+    });
+}, {
+    threshold:0.2,
+})
+
+elements.forEach((element,i) =>{
+    if(i%2 === 0){
+        element.classList.add("left-to-right");
+        observer.observe(element)
+    } else {
+        element.classList.add("right-to-left");
+        observer.observe(element)
+    }
+});
+
